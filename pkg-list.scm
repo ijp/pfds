53ffd48ed263d16c5de0753bea9853cb3a4e2ea7
Ian Price <ianprice90@googlemail.com>
2012-11-23 14:08:23 +0000

    Update pkg-list.scm for v0.2

--- pkg-list.scm
+++ pkg-list.scm
@@ -1,10 +1,22 @@
-(package (pfds (0 1))
+(package (pfds (0 2))
   (depends (wak-trc-testing))
   (synopsis "Purely Functional Data Structures")
   (description
-   "A library of data structures for functional programmers (eventually)."
-   "Right now just queues and deques.")
+   "A library of data structures for functional programmers."
+   "It contains implementations of:"
+   "- queues"
+   "- deques"
+   "- bbtrees"
+   "- sets"
+   "- dlists"
+   "- priority search queues"
+   "- heaps"
+   "- finger trees"
+   "- sequences")
   (homepage "http://github.com/ijp/pfds")
+  (documentation
+   "README.org"
+   "LICENSE")
   (libraries
    (sls -> "pfds")
    ("private" -> ("pfds" "private"))))
